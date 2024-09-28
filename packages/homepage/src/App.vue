721fe9808e72e677fe38d763bec9e9efba4235e6
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-09-28 14:17:29 +0900

    Add: 背景画像を追加

--- packages/homepage/src/App.vue
+++ packages/homepage/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useHead } from "@unhead/vue"
 import LandingTexts from "./components/LandingTexts.vue"
+import demo from "./assets/demo.png"
 
 useHead({
   meta: [
@@ -40,6 +41,22 @@ useHead({
 
 <template>
   <main class="relative"><LandingTexts /></main>
+  <img :src="demo" class="demo-background" />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+main {
+  z-index: 1;
+}
+.demo-background {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 0.1;
+
+  pointer-events: none;
+}
+</style>
