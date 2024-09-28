05d607071510c9446f4fac2d44d054fc7cc4c931
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-09-29 07:51:13 +0900

    Add: (dev)を追加

--- packages/frontend/src/components/BuildInfo.vue
+++ packages/frontend/src/components/BuildInfo.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 const buildInfo = import.meta.env.VITE_COMMIT
+const isDev = import.meta.env.DEV
 </script>
 <template>
   <div
     class="absolute left-3 bottom-3 hidden xs:block opacity-50 pointer-events-none"
   >
-    Build: {{ buildInfo }}
+    Build: {{ buildInfo }}{{ isDev ? " (dev)" : "" }}
   </div>
 </template>
 <style scoped lang="scss"></style>
